1cd83d9c896ed5d451e0fcf6b3c794a4ceaca15b
AV Mohith <83958110+mohith-av@users.noreply.github.com>
2024-05-29 19:04:21 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Your message has been sent!');
+    // Here you can add more functionality like sending the form data to a server
+});
